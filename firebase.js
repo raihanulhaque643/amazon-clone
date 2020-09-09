d588eee4444ddb14695ee5ef6b87a5f195931d90
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-09 14:59:25 +0600

    header/navbar done

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDxjneMkAE7ULudhkaomvEak2Jv0vISY4M",
+    authDomain: "clone-653ce.firebaseapp.com",
+    databaseURL: "https://clone-653ce.firebaseio.com",
+    projectId: "clone-653ce",
+    storageBucket: "clone-653ce.appspot.com",
+    messagingSenderId: "641078700275",
+    appId: "1:641078700275:web:f91e446695b5fc936baf86"
+  };
