f57af4a4875d3318c95d0bc77e46e90d61947e2a
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-10 17:36:58 +0600

    user auth using firebase implemented

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Checkout from './components/Checkout';
+import Login from './components/Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
+ 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        // user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+   
   return (
     <Router>
       <div className="App">
-      <Header />
       <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
         <Route path="/checkout">
+          <Header />
           <Checkout />
         </Route>
         <Route path="/">
+          <Header />
           <Home />
         </Route>
       </Switch>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDxjneMkAE7ULudhkaomvEak2Jv0vISY4M",
     authDomain: "clone-653ce.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
     messagingSenderId: "641078700275",
     appId: "1:641078700275:web:f91e446695b5fc936baf86"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
